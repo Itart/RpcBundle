d3e14e2ccda4a3c5ac3d22e26c7729120b0a9d7f
Pavel Gopanenko <pavelgopanenko@gmail.com>
2012-01-18 04:47:48 +0700

    Add service comments

--- Service/RpcService.php
+++ Service/RpcService.php
@@ -1,18 +1,47 @@
 <?php
 
+/*
+ * This file is part of the RPC Bundle for Symfony2.
+ *
+ * (c) Pavel Gopanenko <pavelgopanenko@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Itart\Bundle\RpcBundle\Service;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * Base RPC Service class.
+ *
+ * @package    ItartRpcBundle
+ * @subpackage Service
+ * @author     Pavel Gopanenko <pavelgopanenko@gmail.com>
+ * @copyright  2012 Pavel Gopanenko
+ */
 abstract class RpcService
 {
     private $_container;
 
+    /**
+     * Construct.
+     *
+     * @param ContainerInterface $container A ContainerInterface instance
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->_container = $container;
     }
 
+    /**
+     * Gets a service.
+     *
+     * @param string $id The service identifier
+     *
+     * @return object The associated service
+     */
     protected function get($id)
     {
         return $this->_container->get($id);
