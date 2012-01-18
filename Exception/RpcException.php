d7551c404bfcbe0a77a7fe10968eb0d0573d14fe
Dmitrij Zakharov <zakharov.dmitrij@gmail.com>
2012-01-18 18:46:27 +0700

    RpcException have been added

---
+++ Exception/RpcException.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the RPC Bundle for Symfony2.
+ *
+ * (c) Dmitrij Zakharov <zakharov.dmitrij@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Itart\Bundle\RpcBundle\Exception;
+
+/**
+ * Base RPC Exception class.
+ *
+ * @package    ItartRpcBundle
+ * @subpackage Exception
+ * @author     Dmitrij Zakharov <zakharov.dmitrij@gmail.com>
+ * @copyright  2012 Dmitrij Zakharov
+ */
+class RpcException extends \Exception
+{
+}
